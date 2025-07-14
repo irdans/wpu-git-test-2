888b41ec49dced84dc543e0c62caab538b8b5a09
irdans <irdan.firdaus32@smk.belajar.id>
2025-07-14 10:39:05 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Coba Git</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias facere expedita ullam nisi quos distinctio quas, nobis explicabo officiis, impedit suscipit ipsam possimus? Hic eligendi rerum incidunt facilis, iure sequi!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+</ul>
 </body>
 </html>
